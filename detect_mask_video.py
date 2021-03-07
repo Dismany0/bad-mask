727589c6b52e44a556875015dab12df91c8fffaf
Dismany0 <45763007+Dismany0@users.noreply.github.com>
2021-03-07 04:39:05 -0500

    Update detect_mask_video.py

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -1,4 +1,3 @@
-
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
@@ -17,11 +16,10 @@ displayeyebox = False
 displaycolourcircle = False
 displayfill = False
 
-def detect_and_predict_mask(frame, faceNet, maskNet):
+def findIfMask(frame, faceNet, maskNet):
 	
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
 
 	faceNet.setInput(blob)
 	detections = faceNet.forward()
@@ -61,28 +59,23 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--face", type=str,
-	default="face_detector",
-	help="path to face detector model directory")
+	default="face_detector")
 ap.add_argument("-m", "--model", type=str,
-	default="mask_detector.model",
-	help="path to trained face mask detector model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
+	default="mask_detector.model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5)
 args = vars(ap.parse_args())
 
-# load our serialized face detector model from disk
-print("[INFO] loading face detector model...")
+
 prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
 weightsPath = os.path.sep.join([args["face"],
 	"res10_300x300_ssd_iter_140000.caffemodel"])
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
-print("[INFO] loading face mask detector model...")
+
 maskNet = load_model(args["model"])
 
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
+
+print("Starting Video")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
@@ -98,14 +91,14 @@ vid = cv2.VideoCapture(best.url)
 
 lower = np.array([103,74,49]) 
 upper = np.array([163,134,109])
-# loop over the frames from the video stream
+
 while True:
 
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
 	
 	try:
-		(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+		(locs, preds) = findIfMask(frame, faceNet, maskNet)
 	except:
 		print("test")
 		pass
@@ -121,7 +114,7 @@ while True:
 
 		ret, vidframe = vid.read()
 		ret, vidframe = vid.read()
-		ret, vidframe = vid.read() # i'll fucking do it again
+		ret, vidframe = vid.read()
 		if (x+int(w/2)<frame.shape[1] and y+int(1.5*h)<frame.shape[0]):
 			pass
 		else:
@@ -158,17 +151,15 @@ while True:
 		label = "Mask Detected" if mask > withoutMask else "Please put on a mask"
 		color = (0, 255, 0) if label == "Mask Detected" else (0, 0, 255)
 
-		# include the probability in the labelq
 		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-		# display the label and bounding box rectangle on the output
-		# frame
+
 		cv2.putText(frame, label, (startX, startY - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 		if( displayfacebox):
 			cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-	# show the output frame
+
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
